8e7fc0678f82a9c26f7a925bc3938ddf647c6f50
Altair <gcthuan@apcs.vn>
2014-06-18 08:38:21 +0700

    Fix heroku

--- db/migrate/20140617015249_create_questions.rb
+++ db/migrate/20140617015249_create_questions.rb
@@ -2,9 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.string :name
-
       t.timestamps
     end
-    add_index :questions, [:package_id]
   end
 end
